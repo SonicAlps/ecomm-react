20af7dc49698d5a48c5140c97ad4edd8c572c22b
SonicAlps <ralphemersonic@gmail.com>
2026-03-03 00:39:27 +0800

    Added Signup and Login form

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export default function Auth(){
-    return <div>Home Page</div>
+    
+    const [mode, setMode] = useState("signup");
+
+    const {
+        register,
+        handleSubmit, formState: { errors }, } = useForm();
+
+    return <div className="page">
+        <div className="container">
+            <div className="auth-container">
+                <h1 className="page-title">
+                    {mode === "signup" ? "Sign Up" : "Login" }
+                </h1>
+                <form className="auth-form">
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="email">Email</label>
+                        <input className="form-input" type="email" id="email" {...register('email', { required: "Email is required" })} />
+                    </div>
+                </form>
+                <form className="auth-form">
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="password">Password</label>
+                        <input className="form-input" type="password" id="password"
+                        {...register('password',
+                        { required: "Password is required",
+                        minLength: {
+                            value: 6,
+                        message: "Password must be at least 6 characters"},
+                        maxLength: {
+                            value: 12,
+                            message: "Password must be less than 12 characters",
+                        },
+                         })}/>
+                    </div>
+
+                    <button type = "submit" className="btn btn-primary btn-large">{mode === "signup" ? "Sign Up" : "Login" }</button>
+
+                    <div className="auth-switch">
+                        {mode === "signup" ? 
+                        (<p>Already have an account? <span className="auth-link" onClick = {() => setMode("login")}>Login</span></p>) 
+                        : 
+                        (<p>Already have an account? <span className="auth-link" onClick = {() => setMode("signup")}>Sign Up</span></p>)}
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
 }
