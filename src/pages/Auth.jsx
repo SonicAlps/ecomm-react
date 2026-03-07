10fe82c1df285a442a860e2d3b7945d1c9bf25ad
SonicAlps <ralphemersonic@gmail.com>
2026-03-07 22:20:54 +0800

    Authentication with useContext

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,27 +1,56 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useForm } from "react-hook-form";
+import { AuthContext } from "../context/AuthContext";
 
 export default function Auth(){
     
     const [mode, setMode] = useState("signup");
+    const [error, setError] = useState(null);
+    const { signUp, user, logout, login } = useContext(AuthContext);
 
     const {
         register,
         handleSubmit, formState: { errors }, } = useForm();
 
+    function onSubmit(data) {
+        setError(null);
+        let result;
+
+        if(mode === "signup"){
+            result = signUp(data.email, data.password);
+        } else {
+            result = login(data.email, data.password);
+        }
+
+        if (result.success){
+            alert('yey')
+        } else {
+            setError(result.error)
+        }
+
+        console.log(result);
+    }
+
     return <div className="page">
         <div className="container">
             <div className="auth-container">
+                {user && <p>User logged in: {user.email}</p>}
+                <button onClick={() => logout()}>Logout</button>
                 <h1 className="page-title">
                     {mode === "signup" ? "Sign Up" : "Login" }
                 </h1>
-                <form className="auth-form">
+                <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+                    {error && <div className="error-message">{error}</div>}
+
+
                     <div className="form-group">
                         <label className="form-label" htmlFor="email">Email</label>
                         <input className="form-input" type="email" id="email" {...register('email', { required: "Email is required" })} />
                     </div>
-                </form>
-                <form className="auth-form">
+                    {errors.email &&  (<span className="form-error">{errors.email.message}</span>)}
+                
+
+                
                     <div className="form-group">
                         <label className="form-label" htmlFor="password">Password</label>
                         <input className="form-input" type="password" id="password"
@@ -35,6 +64,7 @@ export default function Auth(){
                             message: "Password must be less than 12 characters",
                         },
                          })}/>
+                         {errors.password &&  (<span>{errors.password.message}</span>)}
                     </div>
 
                     <button type = "submit" className="btn btn-primary btn-large">{mode === "signup" ? "Sign Up" : "Login" }</button>
